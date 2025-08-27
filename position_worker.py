443e8c362f3aad92a20f9f92cf9d369dfa703b41
Israel Cabrera <icabrera@arkusnexus.com>
2025-08-26 19:05:02 -0700

    ...

--- position_worker.py
+++ position_worker.py
@@ -7,14 +7,10 @@ import numpy as np
 import logging
 
 # Door camera parameters:
-# fx, fy, cx, cy = (np.float64(581.685594957565), np.float64(579.6227773128813), np.float64(639.5000000004447), np.float64(359.4999999990846))
-# fx, fy, cx, cy = (np.float64(636.4083958013254), np.float64(702.358399179285), np.float64(970.3430712575822), np.float64(447.94837834538697))
-# fx, fy, cx, cy = (np.float64(636.4083958013254), np.float64(702.358399179285), np.float64(970.3430712575822), np.float64(447.94837834538697))
-# fx, fy, cx, cy = (np.float64(434.0708275610782), np.float64(435.3281639557073), np.float64(639.4999999917694), np.float64(359.50000000946426))
+# TODO: user calibrate_camera_2
 
 # Laptop web cam
-# fx, fy, cx, cy = (np.float64(777.1974361141605), np.float64(778.8196369288024), np.float64(319.50000001093974), np.float64(239.49999998490622))
-# fx, fy, cx, cy = (np.float64(620.3821303011521), np.float64(618.9357863151845), np.float64(319.5000000048588), np.float64(239.50000001303846))
+# fx, fy, cx, cy = (np.float64(651.4091046043549), np.float64(653.2336134231156), np.float64(318.32692349087426), np.float64(234.8164852829485))
 
 class PositionWorker:
     def __init__(self, num_workers=2):
